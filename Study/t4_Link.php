b7e00cc96b6f8670ecb15c5a20fc1c2fce429bee
문주한 <codemike@naver.com>
2018-04-22 10:46:51 +0000

    t1

---
+++ Study/t4_Link.php
@@ -0,0 +1,84 @@
+<html>
+	<head>
+		<title>링크</title>
+	</head>
+	<body>
+		<h1 id="top">
+			영화인들
+		</h1>
+		
+		<h2>
+			영화 일기
+		</h2>
+		<p>
+			올해 참석할 영화제는 다음과 같습니다.
+			<br />
+			더 자세한 정보를 원하신다면 <a href="mailto:flimolk@example.org">연락</a>주세요.
+		</p>
+		
+		<h3>
+			1월
+		</h3>
+		<p>
+			<a href="http://www.sundance.org">선댄스 영화제(Sundance Flim Festival)</a>
+			<br />
+			파크 시티, 유타, 미국
+			<br />
+			2011년 1월 20일 - 30일
+		</p>
+		
+		<h3>
+			2월
+		</h3>
+		<p>
+			<a href="http://www.tropfest.com">트로페스트(Tropfest)</a>
+			<br />
+			시드니, 호주
+			<br />
+			2011년 2월 20일
+		</p>
+		
+		<h3>
+			3월
+		</h3>
+		<p>
+			<a href="http://www.sxsw.com">사우스 바이 사우스웨스트(South by Southwest)</a>
+			<br />
+			오스틴, 텍사스, 미국
+			<br />
+			2011년 3월 11일 - 20일
+		</p>
+		
+		<h3>
+			4월
+		</h3>
+		<p>
+			<a href="http://londonindependent.org">런던 독립 영화제(London Independent Flim Festival)</a>
+			<br />
+			런던, 영국
+			<br />
+			2011년 4월 15일 - 24일
+		</p>
+		
+		<h3>
+			5월
+		</h3>
+		<p>
+			<a href="http://CannFlim.test">칸 국제 영화제(Canners International Flim Festival)</a>
+			<br />
+			칸, 프랑스
+			<br />
+			2011년 5월 11일 - 22일
+		</p>
+		
+		<p>
+			<a href="t6.php">영화인들 소개</a>
+		</p>
+		<p>
+			<a href="#top">맨 위로</a>
+		</p>
+	</body>
+</html>
+<!--
+<a> = 링크 href = 주소 해당 사이트로 이동 외에도 id로 이동 가능
+-->
---
+++ Study/t5_image.php
@@ -0,0 +1,48 @@
+<html>
+	<head>
+		<title>이미지</title>
+	</head>
+	<body>
+		<h1>
+			<img src="http://htmlandcssbook.com/code-samples/chapter-05/images/logo.gif">
+		</h1>
+		
+		<figure>
+			<img src="http://htmlandcssbook.com/code-samples/chapter-05/images/chocolate-islands.jpg"
+				 alt="초콜릿 아일랜드(Chocoalte Islands)"
+				 title="초콜릿 케익" />
+			<p>
+				<figcaption>
+					간단하고 맛있는 초콜릿 케익 요리법
+				</figcaption>
+			</p>
+		</figure>
+		
+		
+		
+		<h4>
+			상세 요리법:
+		</h4>
+		<p>
+			<img src="http://htmlandcssbook.com/code-samples/chapter-05/images/lemon-posset.jpg"
+				 alt ="레몬 포셋(Lemon Posset)"
+				 title="레몬 포셋(Lemon Posset)" />
+			<img src="http://htmlandcssbook.com/code-samples/chapter-05/images/roasted-brussel-sprouts.jpg"
+				 alt="구운 브뤼셀 스프라우트(Roasted Brussel Sprouts)"
+				 title="구운 브뤼셀 스프라우트(Roasted Brussel Sprouts)" />
+			<img src="http://htmlandcssbook.com/code-samples/chapter-05/images/zucchini-cake.jpg"
+				 alt="주키니 케이크(Zucchini cake)"
+				 title="주키니 케이크(Zucchini cake)" />
+		</p>
+		
+	</body>
+</html>
+<!--
+<figure> = 이미지와 캡션을 포함시켜주는 요소
+<figcaption> = 이미지의 해당 캡션을 연결
+
+<img /> = 이미지 태그
+src = 이미지 소스 주소
+alt = 시각장애인용 음성 인식 속성
+title = 마우스 팝업 속성
+-->
