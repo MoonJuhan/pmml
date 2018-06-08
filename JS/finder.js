7e08928c02ee0b69a65731fed71732438ecd3ce2
문주한 <codemike@naver.com>
2018-06-08 14:36:08 +0000

    입력시 텍스트 수정

--- JS/finder.js
+++ JS/finder.js
@@ -40,6 +40,14 @@ var example2 = new Vue({
           in1++;
           this.assignment = entry[in1].content.$t;
         }
+		  else{
+			  this.stuID = "...";
+			  this.dday = "000";
+			  this.inday = "00.00.00";
+			  this.outday = "00.00.00";
+			  this.regiment = "...";
+			  this.assignment = "...";
+		  }
       }
     }
   }
