3526b267f5de16d0e8498b1ec6be6b2f0e3b96e5
문주한 <codemike@naver.com>
2018-06-29 14:19:06 +0000

    Design task ing

--- JS/finder.js
+++ JS/finder.js
@@ -1,4 +1,5 @@
-var GSScurl = "https://spreadsheets.google.com/feeds/cells/14pDCMfhYCyTRfsl_RaWa-_viDwsNpGyLSRhHFMH3R1s/1/public/basic?alt=json-in-script&min-row=1&min-col=1&callback=?";
+var GSScurl =
+    'https://spreadsheets.google.com/feeds/cells/14pDCMfhYCyTRfsl_RaWa-_viDwsNpGyLSRhHFMH3R1s/1/public/basic?alt=json-in-script&min-row=1&min-col=1&callback=?';
 var entry;
 var member_list = new Array();
 
@@ -6,20 +7,18 @@ var member_list = new Array();
 var testob = new Object();
 testob['문주한'] = null;
 
-
 $.getJSON(GSScurl, function(data) {
     entry = data.feed.entry;
-    //구글 스프레드 시트의 모든 내용은 feed.entry에 담겨있습니다.	
+    //구글 스프레드 시트의 모든 내용은 feed.entry에 담겨있습니다.
     var x = 0;
 
     for (var i in entry) {
         if (entry[i].title.$t.substring(0, 1) == 'A') {
             if (entry[i].title.$t != 'A1' && entry[i].title.$t != 'A2') {
-
                 member_list[x] = new Object();
                 member_list[x].name = entry[i].content.$t;
-				testob[member_list[x].name] = null;
-				
+                testob[member_list[x].name] = null;
+
                 var y = i;
                 for (var z = 0; z < 8; z++) {
                     switch (entry[y].title.$t.substring(0, 1)) {
@@ -50,7 +49,6 @@ $.getJSON(GSScurl, function(data) {
     }
 });
 
-
 var app = new Vue({
     el: '#app',
     data: {
@@ -64,19 +62,19 @@ var app = new Vue({
         gagestyle: {
             width: '0%'
         }
-
     },
     // 메소드는 `methods` 객체 안에 정의합니다
     methods: {
         updateValue: function(value) {
-			this.name = value;
+            this.name = value;
 
-			$('input.autocomplete').autocomplete({
-				data: testob,
-				limit: 5, // The max amount of results that can be shown at once. Default: Infinity.
-			});
-			
-            for (var i = 0; i < member_list.length; i++) { // 각 행에대해 아래 스크립트를 실행합니다.
+            $('input.autocomplete').autocomplete({
+                data: testob,
+                limit: 5 // The max amount of results that can be shown at once. Default: Infinity.
+            });
+
+            for (var i = 0; i < member_list.length; i++) {
+                // 각 행에대해 아래 스크립트를 실행합니다.
                 if (member_list[i].name == value) {
                     this.stuID = member_list[i].stuID;
                     this.dday = member_list[i].dday;
@@ -99,7 +97,6 @@ var app = new Vue({
 
                     var percent = (allday - this.dday) / allday * 100;
                     this.gagestyle.width = percent + '%';
-
                 }
             }
         }
@@ -108,10 +105,3 @@ var app = new Vue({
 
 console.log(testob);
 console.log(member_list);
-
-
-    
-
-	
-
-
--- Study/t9j.js
+++ Study/t9j.js
@@ -1,10 +1,12 @@
-var GSScurl = "https://spreadsheets.google.com/feeds/cells/14pDCMfhYCyTRfsl_RaWa-_viDwsNpGyLSRhHFMH3R1s/1/public/basic?alt=json-in-script&min-row=1&min-col=1&callback=?";
+var GSScurl =
+    'https://spreadsheets.google.com/feeds/cells/14pDCMfhYCyTRfsl_RaWa-_viDwsNpGyLSRhHFMH3R1s/1/public/basic?alt=json-in-script&min-row=1&min-col=1&callback=?';
 var entry;
 var member_list = new Array();
 
 // Router Test
 var Header = {
-	template : '<header class="no-padding">\
+    template:
+        '<header class="no-padding">\
         <div class="navbar-fixed">\
             <nav class="navfeature #37474f blue-grey darken-1">\
                 <div class="container nav-wrapper">\
@@ -14,7 +16,7 @@ var Header = {
                     <ul id="nav" class="hide-on-med-and-down right">\
                         <li><router-link class="navmenu" to="/about">About</router-link></li>\
                         <li><router-link class="navmenu" to="/list">List</router-link></li>\
-                        <li><a class="navmenu" href="https://docs.google.com/spreadsheets/d/14pDCMfhYCyTRfsl_RaWa-_viDwsNpGyLSRhHFMH3R1s/edit?usp=sharing">Edit</a>\</li>\
+                        <li><a class="navmenu" href="https://docs.google.com/spreadsheets/d/14pDCMfhYCyTRfsl_RaWa-_viDwsNpGyLSRhHFMH3R1s/edit?usp=sharing">Edit</a></li>\
                     </ul>\
                 </div>\
             </nav>\
@@ -23,63 +25,61 @@ var Header = {
 };
 
 var Footer = {
-	template : '<footer class="page-footer #bdbdbd grey lighten-1">\
+    template:
+        '<footer class="page-footer #bdbdbd grey lighten-1">\
         <div class="footer-copyright">\
             <div class="container">\
                 © 2014 Copyright Text\
                 <a class="grey-text text-lighten-4 right" href="#!">More Links</a>\
             </div>\
         </div>\
-    </footer>'	
+    </footer>'
 };
 
 var routes = [
-	{	
-		path: '/',	
-		components: {	
-			header : Header,	
-			footer : Footer
-		}
-	},
-	{
-		path: '/about',
-		components: {
-			header : Header,
-			footer : Footer
-		}
-	},
-	{
-		path: '/list',
-		components: {
-			header : Header,
-			footer : Footer
-		}
-	}
+    {
+        path: '/',
+        components: {
+            header: Header,
+            footer: Footer
+        }
+    },
+    {
+        path: '/about',
+        components: {
+            header: Header,
+            footer: Footer
+        }
+    },
+    {
+        path: '/list',
+        components: {
+            header: Header,
+            footer: Footer
+        }
+    }
 ];
 
-
 var router = new VueRouter({
-	routes
+    routes
 });
 
 // Autocomplete Test
 var testob = new Object();
 testob['문주한'] = null;
 
-
 $.getJSON(GSScurl, function(data) {
     entry = data.feed.entry;
-    //구글 스프레드 시트의 모든 내용은 feed.entry에 담겨있습니다.	
+    //구글 스프레드 시트의 모든 내용은 feed.entry에 담겨있습니다.
     var x = 0;
 
     for (var i in entry) {
         if (entry[i].title.$t.substring(0, 1) == 'A') {
             if (entry[i].title.$t != 'A1' && entry[i].title.$t != 'A2') {
-
                 member_list[x] = new Object();
                 member_list[x].name = entry[i].content.$t;
-				testob[member_list[x].name] = null;
-				
+                testob[member_list[x].name] = null;
+
                 var y = i;
                 for (var z = 0; z < 8; z++) {
                     switch (entry[y].title.$t.substring(0, 1)) {
@@ -110,10 +110,9 @@ $.getJSON(GSScurl, function(data) {
     }
 });
 
-
 var app = new Vue({
     el: '#app',
-	router: router,
+    router: router,
     data: {
         name: '이름',
         stuID: '학번',
@@ -125,21 +124,21 @@ var app = new Vue({
         gagestyle: {
             width: '0%'
         }
-
     },
     // 메소드는 `methods` 객체 안에 정의합니다
     methods: {
         updateValue: function() {
-          // alert("hello");
-            var name2 = document.getElementById("name1").value;
+            // alert("hello");
+            var name2 = document.getElementById('name1').value;
             this.name = name2;
 
-			$('input.autocomplete').autocomplete({
-        data: testob, 
-        limit: 5, // The max amount of results that can be shown at once. Default: Infinity.
-      });
-			
-            for (var i = 0; i < member_list.length; i++) { // 각 행에대해 아래 스크립트를 실행합니다.
+            $('input.autocomplete').autocomplete({
+                data: testob,
+                limit: 5 // The max amount of results that can be shown at once. Default: Infinity.
+            });
+
+            for (var i = 0; i < member_list.length; i++) {
+                // 각 행에대해 아래 스크립트를 실행합니다.
                 if (member_list[i].name == name2) {
                     this.stuID = member_list[i].stuID;
                     this.dday = member_list[i].dday;
@@ -162,16 +161,15 @@ var app = new Vue({
 
                     var percent = (allday - this.dday) / allday * 100;
                     this.gagestyle.width = percent + '%';
-
                 }
             }
         },
-		clickbtn: function(){
-			alert("버튼 클릭!");
-		}
+        clickbtn: function() {
+            alert('버튼 클릭!');
+        }
     }
 });
-	/*
+/*
 	$(document).ready(function() {
       $('input.autocomplete').autocomplete({
         data: testob, 
@@ -181,10 +179,3 @@ var app = new Vue({
 */
 console.log(testob);
 console.log(member_list);
-
-
-    
-
-	
-
-
