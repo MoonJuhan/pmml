cb0c49f2ff68ea926bbe44b80663e8798c5f0e32
문주한 <codemike@naver.com>
2018-08-04 13:22:15 +0000

    fix - template 수정

--- vrouter.js
+++ vrouter.js
@@ -1,23 +1,9 @@
 var body1 = {
-	template: '<div class="col l12 m12 s12">\
-					<div class="aboutCard">\
-						<span class="cardTitle">About</span>\
-						<div class="cardContents">\
-							this is pmml!\
-						</div>\
-					</div>\
-				</div>'
+	template: '<div class="col l12 m12 s12"><div class="aboutCard"><span class="cardTitle">About</span><div class="cardContents">this is pmml!</div></div></div>'
 };
 
 var body2 = {
-	template: '<div class="col l12 m12 s12">\
-					<div class="aboutCard">\
-						<span class="cardTitle">About</span>\
-						<div class="cardContents">\
-							this is body2!\
-						</div>\
-					</div>\
-				</div>'
+	template: '<div class="col l12 m12 s12"><div class="aboutCard"><span class="cardTitle">About</span><div class="cardContents">this is body!</div></div></div>'
 };
 
 var router = new VueRouter({
