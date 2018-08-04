af002972bac84fc077ecb5773ba3fc275c0aa719
문주한 <codemike@naver.com>
2018-08-04 13:16:02 +0000

    feat - Vue Router 테스트

---
+++ vrouter.js
@@ -0,0 +1,39 @@
+var body1 = {
+	template: '<div class="col l12 m12 s12">\
+					<div class="aboutCard">\
+						<span class="cardTitle">About</span>\
+						<div class="cardContents">\
+							this is pmml!\
+						</div>\
+					</div>\
+				</div>'
+};
+
+var body2 = {
+	template: '<div class="col l12 m12 s12">\
+					<div class="aboutCard">\
+						<span class="cardTitle">About</span>\
+						<div class="cardContents">\
+							this is body2!\
+						</div>\
+					</div>\
+				</div>'
+};
+
+var router = new VueRouter({
+	routes: [
+		{
+		path: '/foo',
+		components: body1
+	},
+		{
+		path: '/bar',
+		components: body2
+	}
+			]
+});
+
+var app = new Vue({
+	el: '#app',
+	router
+});
