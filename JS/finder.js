f78e7fea024bd4382a21831c5aa1275bb61e5de5
문주한 <codemike@naver.com>
2018-08-04 13:45:16 +0000

    fix - VRT1 html 으로 테스트 이동

--- JS/finder.js
+++ JS/finder.js
@@ -83,40 +83,8 @@ Vue.component('modal', {
 	template: '#modal-template'
 });
 
-var body1 = {
-	template:
-		'<div class="col l12 m12 s12"><div class="aboutCard"><span class="cardTitle">About</span><div class="cardContents">this is pmml!</div></div></div>'
-};
-
-var body2 = {
-	template:
-		'<div class="col l12 m12 s12"><div class="aboutCard"><span class="cardTitle">About</span><div class="cardContents">this is body!</div></div></div>'
-};
-
-var body3 = {
-	template: '#vueroutertest'
-};
-
-var router = new VueRouter({
-	routes: [
-		{
-			path: '/foo',
-			components: body1
-		},
-		{
-			path: '/bar',
-			components: body2
-		},
-		{
-			path: '/vrt',
-			components: body3
-		}
-	]
-});
-
 var app = new Vue({
 	el: '#app',
-	router,
 	data: {
 		name: '',
 		stuID: '00학번',
--- vrouter.js
+++ vrouter.js
@@ -1,11 +1,11 @@
 var body1 = {
 	template:
-		'<div class="col l12 m12 s12"><div class="aboutCard"><span class="cardTitle">About</span><div class="cardContents">this is pmml!</div></div></div>'
+		'<p>im body1</p>'
 };
 
 var body2 = {
 	template:
-		'<div class="col l12 m12 s12"><div class="aboutCard"><span class="cardTitle">About</span><div class="cardContents">this is body!</div></div></div>'
+		'<p>im body2</p>'
 };
 
 var body3 = {
