1d0b1549367a7728310207f29357fc8df25606dc
moonjuhan <codemike@naver.com>
2019-01-16 16:53:00 +0900

    fix - Footer 크기 조절 및 Mobile 화면 적용

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,8 @@
 <template lang="html">
   <v-footer class="pa-3 footer pageFooter">
-    <h1>PMML</h1>
-    <v-spacer></v-spacer>
-    <div>Copyright &copy; {{ new Date().getFullYear() }} MoonJuhan. All rights reserved.</div>
+    <h1 class="hidden-sm-and-down">PMML</h1>
+    <v-spacer class="hidden-sm-and-down"></v-spacer>
+    <div class="footerCopyright">Copyright 2019. MoonJuhan. <br class="hidden-sm-and-up">All rights reserved.</div>
   </v-footer>
 </template>
 
@@ -15,8 +15,15 @@ export default {
 /* Footer */
 .pageFooter {
   background-color: rgba(90, 98, 101, 0.8) !important;
-  padding-top: 0px !important;
+  padding-top: 4px !important;
+  padding-bottom: 4px !important;
   height: auto !important;
   color: white;
 }
+@media screen and (max-width:960px) {
+  .footerCopyright{
+    width: 100%;
+  }
+}
+
 </style>
